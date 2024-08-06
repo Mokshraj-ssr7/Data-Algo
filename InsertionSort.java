6159457c58265f2e18643083e5f87c7049d0f222
Mokshraj(ssr7) <83112088+Mokshraj-ssr7@users.noreply.github.com>
2024-08-06 09:24:00 +0530

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,39 @@
+public class InsertionSort {
+
+    // Function to perform insertion sort
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; // The element to be inserted
+            int j = i - 1;
+            
+            // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    // Function to print an array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        
+        System.out.println("Unsorted array: ");
+        printArray(arr);
+        
+        insertionSort(arr);
+        
+        System.out.println("Sorted array: ");
+        printArray(arr);
+    }
+}
